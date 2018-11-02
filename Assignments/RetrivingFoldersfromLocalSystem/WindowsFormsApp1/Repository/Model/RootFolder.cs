21f95dab20815e9278f2c50dcf1d3751fb863ada
Bharath-Naidu <nbharat229@gmail.com>
2018-11-02 15:15:05 +0530

    FinalProjectPart

---
+++ Assignments/RetrivingFoldersfromLocalSystem/WindowsFormsApp1/Repository/Model/RootFolder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    class RootFolder
+    {
+        int  Id { get; set; }
+        string Name { get; set; }
+        string Path { get; set; }
+        string CreatedBy { get; set; }
+        string ModifiedBy { get; set; }
+        DateTime CreatedOn { get; set; }
+        DateTime ModifiedOn { get; set; }
+    }
+}
